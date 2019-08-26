96285158f41a7f6854ef68949ac2d43871eb7dcf
zhangyifeng <291323003@qq.com>
2019-08-26 22:45:20 +0800

    up

--- src/Characteristic/Base.php
+++ src/Characteristic/Base.php
@@ -8,4 +8,21 @@ trait Base
 {
     private $data = [];
     private $schemaInfo = [];
+
+    function __construct(?array $data = null)
+    {
+        if($data){
+            $this->setData($data);
+        }
+    }
+
+    public static function create(array $data = null)
+    {
+        return new static($data);
+    }
+
+    public function setData(array $data)
+    {
+
+    }
 }
