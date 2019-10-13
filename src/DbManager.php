7dab1c495d73814a1bcb0cadb6c5136b925cfd09
如果的如果 <admin@fosuss.com>
2019-10-13 20:42:21 +0800

    新增事务支持

--- src/DbManager.php
+++ src/DbManager.php
@@ -28,27 +28,55 @@ class DbManager
     function getConnection(string $connectionName = 'default'):?ConnectionInterface
     {
         if(isset($this->connections[$connectionName])){
-            return  $this->connections[$connectionName];
+            return $this->connections[$connectionName];
         }
         return null;
     }
 
-    public function startTransaction($atomic = false,$connectionNames = 'default'):bool
+    public function startTransaction($connectionNames = 'default'):bool
     {
+        $successes = [];
+        if(!is_array($connectionNames)){
+            $connectionNames = [$connectionNames];
+        }
+        /*
+         * 1、 raw执行 start transaction
+         * 2、若全部链接执行成功，则往transactionContext 标记对应协程的成功事务，并注册一个defer自动执行回滚,防止用户忘了提交导致死锁
+         * 3、若部分链接成功，则成功链接执行rollback
+         */
+        Coroutine::defer(function (){
+           $cid = Coroutine::getCid();
+           if(isset($this->transactionContext[$cid])){
+               $this->rollback();
+           }
+        });
 
     }
 
-    /*
-     * 强制提交
-     */
-    public function commit($forceAtomic = false):bool
+    public function commit():bool
     {
-
+        Coroutine::defer(function (){
+            $cid = Coroutine::getCid();
+            if(isset($this->transactionContext[$cid])){
+                /*
+                 * raw执行 commit
+                 */
+                unset($this->transactionContext[$cid]);
+            }
+        });
     }
 
     public function rollback():bool
     {
-
+        Coroutine::defer(function (){
+            $cid = Coroutine::getCid();
+            if(isset($this->transactionContext[$cid])){
+                /*
+                 * raw执行 rollback
+                 */
+                unset($this->transactionContext[$cid]);
+            }
+        });
     }
 
 }
