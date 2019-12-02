db976ffcda1bc4e60406619f341a1dc6f96a1f42
Siam <59419979@qq.com>
2019-12-02 20:54:46 +0800

    orm抛出异常

--- tests/ErrorTest.php
+++ tests/ErrorTest.php
@@ -12,8 +12,14 @@ use EasySwoole\Mysqli\QueryBuilder;
 use EasySwoole\ORM\Db\Config;
 use EasySwoole\ORM\Db\Connection;
 use EasySwoole\ORM\DbManager;
+use EasySwoole\ORM\Exception\Exception;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * ErrorTest需要单独运行测试
+ * Class ErrorTest
+ * @package EasySwoole\ORM\Tests
+ */
 class ErrorTest extends TestCase
 {
     /**
@@ -34,11 +40,17 @@ class ErrorTest extends TestCase
 
     public function testGet()
     {
-        $model = TestUserModel::create();
-        $testUserModel = $model->where("xxx", 1)->get("");
-        $this->assertFalse($testUserModel);
-        if ($model->lastQueryResult()->getLastErrorNo() !== 0 ){
-            $this->assertIsString($model->lastQueryResult()->getLastError());
+        try {
+            $model = TestUserModel::create();
+            $testUserModel = $model->where("xxx", 1)->get("");
+            $this->assertFalse($testUserModel);
+            if ($model->lastQueryResult()->getLastErrorNo() !== 0 ){
+                $this->assertIsString($model->lastQueryResult()->getLastError());
+            }
+        } catch (\EasySwoole\Mysqli\Exception\Exception $e) {
+        } catch (Exception $e) {
+            $this->assertEquals("SQLSTATE[42S22] [1054] Unknown column 'xxx' in 'where clause'", $e->getMessage());
+        } catch (\Throwable $e) {
         }
 
     }
@@ -46,10 +58,14 @@ class ErrorTest extends TestCase
     public function testSave()
     {
         // insert 不存在的字段
-        $model = TestUserModel::create();
-        $model->test = 123;
-        $res = $model->save(false , false);
-        $this->assertFalse($res);
+        try {
+            $model = TestUserModel::create();
+            $model->test = 123;
+            $res = $model->save(false, false);
+        } catch (Exception $e) {
+            $this->assertEquals("SQLSTATE[42S22] [1054] Unknown column 'test' in 'field list'", $e->getMessage());
+        } catch (\Throwable $e) {
+        }
     }
 
     // 全部字段为null  id自增
@@ -58,5 +74,19 @@ class ErrorTest extends TestCase
         // $model = TestUserListModel::create();
         // $res = $model->save();
         // $this->assertIsInt($res);
+    }
+
+    public function testThrow()
+    {
+        // 不存在的字段 where 抛出异常
+        try {
+            $test = TestUserModel::create()->get([
+                'fuck_life' => 1
+            ]);
+        }catch (Exception $e) {
+            $this->assertEquals("SQLSTATE[42S22] [1054] Unknown column 'fuck_life' in 'where clause'", $e->getMessage());
+        } catch (\Throwable $e) {
+        }
+
     }
 }
