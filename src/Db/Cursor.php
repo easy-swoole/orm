fef47b1936b64ade5b1398378bb55982b2f6e946
hlhill <554802208@qq.com>
2020-01-16 11:14:44 +0800

    增加orm的cursor功能，在select及all方法的返回结果中可以采用cursor->fetch()的方式迭代获取数据

---
+++ src/Db/Cursor.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haoxu
+ * Date: 2020-01-15
+ * Time: 16:44
+ */
+
+namespace EasySwoole\ORM\Db;
+
+use EasySwoole\ORM\AbstractModel;
+use EasySwoole\ORM\Exception\Exception;
+use phpDocumentor\Reflection\Types\This;
+use Swoole\Coroutine\MySQL\Statement;
+
+class Cursor implements CursorInterface
+{
+    protected $statement;
+    protected $modelName;
+    protected $returnAsArray;
+
+    public function __construct(Statement $statement)
+    {
+        $this->statement = $statement;
+    }
+
+    public function setReturnAsArray(bool $returnAsArray)
+    {
+        $this->returnAsArray = $returnAsArray;
+    }
+
+    public function setModelName(string $modelName)
+    {
+        $this->modelName = $modelName;
+    }
+
+    /**
+     * @return mixed
+     * @throws \Throwable
+     */
+    public function fetch()
+    {
+        try{
+            $data = $this->statement->fetch();
+            if (!$this->returnAsArray && $data) {
+                if (is_null($this->modelName)) {
+                    throw new Exception('ModelName can not be null');
+                }
+                $model = new $this->modelName($data);
+                return $model;
+            }
+            return $data;
+        }catch (\Throwable $throw){
+            throw $throw;
+        }
+    }
+}
---
+++ src/Db/CursorInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: haoxu
+ * Date: 2020-01-15
+ * Time: 16:49
+ */
+
+namespace EasySwoole\ORM\Db;
+
+
+use Swoole\Coroutine\MySQL\Statement;
+
+interface CursorInterface
+{
+    public function __construct(Statement $statement);
+
+    public function setReturnAsArray(bool $returnAsArray);
+
+    public function setModelName(string $modelName);
+
+    public function fetch();
+}
