0d2f8dc037cdf39564e31b503f7a0d6a7f9d57e0
宣言 <59419979@qq.com>
2020-02-28 11:24:36 +0800

    fix

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -1141,7 +1141,7 @@ abstract class AbstractModel implements ArrayAccess, JsonSerializable
                     if(isset($targetTableAlias)){
                         // 如果有包含附属别名，则是targetData
                         if (strpos($key, $targetTableAlias) !==  false){
-                            $trueKey = ltrim($key, $targetTableAlias."_");
+                            $trueKey = substr($key,strpos($key,$targetTableAlias."_")+ strlen($targetTableAlias) + 1);
                             $targetData[$trueKey] = $value;
                         }else{
                             $originData[$key] = $value;
