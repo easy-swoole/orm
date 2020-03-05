f73f11a21936ca46389dde012cb21fb1cb6b07e6
Siam <59419979@qq.com>
2020-03-05 23:34:58 +0800

    优化

---
+++ tests/models/UserRoleDifferentField.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Siam
+ * Date: 2020/2/27
+ * Time: 10:01
+ */
+
+namespace EasySwoole\ORM\Tests\models;
+
+
+use EasySwoole\ORM\AbstractModel;
+
+class UserRoleDifferentField extends AbstractModel
+{
+    protected $tableName='user_role_different_field';
+}
