45abef4851eb0f8d9f23612affcdefc38e0338cc
XueSi <1592328848@qq.com>
2021-09-03 11:54:05 +0800

    fix:add model UserInfo

---
+++ tests/models/UserInfo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of EasySwoole.
+ *
+ * @link https://www.easyswoole.com
+ * @document https://www.easyswoole.com
+ * @contact https://www.easyswoole.com/Preface/contact.html
+ * @license https://github.com/easy-swoole/easyswoole/blob/3.x/LICENSE
+ */
+
+namespace EasySwoole\ORM\Tests\models;
+
+use EasySwoole\ORM\AbstractModel;
+
+class UserInfo extends AbstractModel
+{
+    protected $tableName = 'user_info';
+}
