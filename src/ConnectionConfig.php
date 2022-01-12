6e100068e3c4d243c0b5b4eac07bc2c5b839291d
如果的如果 <admin@fosuss.com>
2022-01-12 12:11:30 +0800

    Changes

--- src/ConnectionConfig.php
+++ src/ConnectionConfig.php
@@ -11,6 +11,26 @@ class ConnectionConfig extends SplBean
      */
     protected $name;
 
+    protected $host;
+    protected $user;
+    protected $password;
+    protected $database;
+    protected $port = 3306;
+    protected $timeout = 45;
+    protected $charset = 'utf8';
+    protected $autoPing = 5;
+
+    //for pool
+    protected $intervalCheckTime = 15*1000;
+    protected $maxIdleTime = 10;
+    protected $maxObjectNum = 20;
+    protected $minObjectNum = 5;
+    protected $getObjectTimeout = 3.0;
+    protected $loadAverageTime = 0.001;
+    protected $extraConf;
+
+
+
     /**
      * @return string|null
      */
--- src/Db/Pool.php
+++ src/Db/Pool.php
@@ -3,6 +3,7 @@
 namespace EasySwoole\ORM\Db;
 
 use EasySwoole\Pool\AbstractPool;
+use EasySwoole\Pool\Config;
 
 class Pool extends AbstractPool
 {
