12ecc4961440709bf70b63450d21c6ef84ed4cb9
如果的如果 <admin@fosuss.com>
2022-01-16 22:22:20 +0800

    fix bug

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -4,5 +4,18 @@ namespace EasySwoole\ORM;
 
 abstract class AbstractModel
 {
+    /** @var RuntimeConfig */
+    private $runtimeConfig;
 
+    function runtimeConfig(?RuntimeConfig $config = null):RuntimeConfig
+    {
+        if($config == null){
+            if($this->runtimeConfig == null){
+                $this->runtimeConfig = new RuntimeConfig();
+            }
+        }else{
+            $this->runtimeConfig = $config;
+        }
+        return $this->runtimeConfig;
+    }
 }
---
+++ src/RuntimeConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace EasySwoole\ORM;
+
+class RuntimeConfig
+{
+
+}
