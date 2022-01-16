1eae0bcf19dee5b7a16f2b35375901fef79e214b
如果的如果 <admin@fosuss.com>
2022-01-16 23:04:58 +0800

    fix bug

--- src/RuntimeConfig.php
+++ src/RuntimeConfig.php
@@ -2,7 +2,38 @@
 
 namespace EasySwoole\ORM;
 
+use EasySwoole\ORM\Db\MysqlClient;
+
 class RuntimeConfig
 {
+    /** @var MysqlClient|null */
+    private $client;
+
+    /**
+     * @var ConnectionConfig $connectionConfig
+     */
+    private $connectionConfig;
+
+    function setConnectionConfig(ConnectionConfig $config):RuntimeConfig
+    {
+        $this->connectionConfig = $config;
+        return $this;
+    }
+
+    private function getClient():MysqlClient
+    {
+        if($this->client){
+            return $this->client;
+        }else{
+            return DbManager::getInstance()->defer($this->connectionConfig->getName());
+        }
+    }
+
+    function setClient(MysqlClient $client):RuntimeConfig
+    {
+        $this->client = $client;
+        return $this;
+    }
+
 
 }
